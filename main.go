244ca1f4b7abcda9b3266b36b1f1e0b180702af3
Zsolt Tasnadi <rastasi@gmail.com>
2019-01-20 14:46:14 +0100

    refactoring, example answers

--- main.go
+++ main.go
@@ -12,8 +12,9 @@ import (
 )
 
 const (
-	VERIFY_TOKEN      = "rubyforever"
-	PAGE_ACCESS_TOKEN = "EAACChyvB7ZCMBAPXK1i9VDFJyPdEEArsMuAuGNFsuCvfe9Sp6LCX21Rkmii580x25GKUVrEYQkx7BDPdOgnibTt9I9jIx4uIgduUbaafmkmiBGn01KxhIU8yKaXjpBQfRZBDhmy2oiP1wsqh0CKgoDZCj6LEGI5mVD3BcZBtBAZDZD"
+	verifyToken     = "rubyforever"
+	pageAccessToken = "EAACChyvB7ZCMBAPXK1i9VDFJyPdEEArsMuAuGNFsuCvfe9Sp6LCX21Rkmii580x25GKUVrEYQkx7BDPdOgnibTt9I9jIx4uIgduUbaafmkmiBGn01KxhIU8yKaXjpBQfRZBDhmy2oiP1wsqh0CKgoDZCj6LEGI5mVD3BcZBtBAZDZD"
+	messagesURL     = "https://graph.facebook.com/v2.6/me/messages"
 )
 
 func main() {
@@ -30,7 +31,7 @@ func getWebhook(w http.ResponseWriter, r *http.Request) {
 	challenge := r.URL.Query()["hub.challenge"][0]
 
 	if mode != "" && token != "" {
-		if mode == "subscribe" && token == VERIFY_TOKEN {
+		if mode == "subscribe" && token == verifyToken {
 			w.Write([]byte(challenge))
 		} else {
 			w.Write([]byte("Forbidden"))
@@ -38,52 +39,75 @@ func getWebhook(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func send_response(sender_id string, text string) {
-	var res = Response{Recipient: Participant{Id: sender_id}, Message: Message{Text: text}}
-
-	res_marshalled, err := json.Marshal(res)
-
+func getJSONResponse(response Response) []byte {
+	jsonResponse, err := json.Marshal(response)
 	if err != nil {
 		fmt.Println(err)
 	}
+	return jsonResponse
+}
 
-	url := "https://graph.facebook.com/v2.6/me/messages"
+func sendResponse(senderID string, text string) {
+	response := Response{Recipient: Participant{Id: senderID}, Message: Message{Text: text}}
+	jsonResponse := getJSONResponse(response)
 
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(res_marshalled))
-	req.Header.Set("Content-Type", "application/json")
+	request, err := http.NewRequest("POST", messagesURL, bytes.NewBuffer(jsonResponse))
+	request.Header.Set("Content-Type", "application/json")
 
-	q := req.URL.Query()
-	q.Add("access_token", PAGE_ACCESS_TOKEN)
-	req.URL.RawQuery = q.Encode()
+	q := request.URL.Query()
+	q.Add("access_token", pageAccessToken)
+	request.URL.RawQuery = q.Encode()
 
 	client := &http.Client{}
-	resp, err := client.Do(req)
+	resp, err := client.Do(request)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
 }
 
-func postWebhook(w http.ResponseWriter, r *http.Request) {
-	b, err := ioutil.ReadAll(r.Body)
+func getBody(w http.ResponseWriter, r *http.Request) []byte {
+	body, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
 	if err != nil {
 		http.Error(w, err.Error(), 500)
-		return
 	}
+	return body
+}
 
+func getAnswer(w http.ResponseWriter, body []byte) Answer {
 	var answer Answer
-	err = json.Unmarshal(b, &answer)
+	err := json.Unmarshal(body, &answer)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
-		return
 	}
+	return answer
+}
+
+func getResponseText(input string) string {
+	if input == "hello" {
+		return "hello!"
+	}
+	if input == "help" {
+		return "Can I help you?"
+	}
+	if input == "contact" {
+		return "www.aiventure.me"
+	}
+	return "Sorry, I don't undersand."
+}
+
+func postWebhook(w http.ResponseWriter, r *http.Request) {
+	body := getBody(w, r)
+	answer := getAnswer(w, body)
 
 	if answer.Object == "page" {
 		event := answer.Entry[0].Messaging[0]
-		sender_id := event.Sender.Id
-		text := event.Message.Text
-		send_response(sender_id, text)
+		if event.Message.Text != "" {
+			senderID := event.Sender.Id
+			text := getResponseText(event.Message.Text)
+			sendResponse(senderID, text)
+		}
 	}
 	w.Write([]byte("EVENT_RECEIVED"))
 }
